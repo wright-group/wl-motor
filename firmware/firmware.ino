1910126eff0dbfe93ae2d01eedf68b37695781ec
Kyle Sunden <sunden@wisc.edu>
2019-05-31 15:54:17 -0500

    Rewrite to simpler firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,97 +1,51 @@
 #include <RotaryEncoder.h>
+#define ENCODER_NUM_STEPS 20
+
 RotaryEncoder encoder(A2, A3);
 const int stepPin = 5;
 const int dirPin = 4;
-int homePin = A6; 
+const int homePin = A6; 
 
-int newPos; //say which direction the translation stage should move 
-int steps = 0;
+long destination; //say which direction the translation stage should move 
+long pos;
+long encoder_position;
 
 void setup() {
-  // put your setup code here, to run once:
+  pos = 0;
+
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 
   pinMode(homePin, INPUT_PULLUP);
 
   Serial.begin(57600);
-  Serial.println("Current position");
-
-  PCICR |= (1 << PCIE1);
-  PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);
-
-
 }
 
-ISR(PCINT1_vect) {
-  encoder.tick(); // just call tick() to check the state.
+void serialEvent(){
+    destination = Serial.parseInt();
 }
 
 void loop() {
-  static int pos = 0;
-
-  int newPos = encoder.getPosition();
-  if (pos < newPos) {
-    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-    // Makes 200 pulses for making one full cycle rotation
-      for(int x = 0; x < 80; x++) {
-        rotate_CCW();
-      }
-    Serial.print(newPos);
-    Serial.println();
-    pos = newPos;
+  encoder.tick();
+  encoder_position = encoder.getPosition();
+  if (encoder_position){
+    destination -= encoder_position * ENCODER_NUM_STEPS;
+    encoder.setPosition(0);
   }
-  else if (pos > newPos) {
-    digitalWrite(dirPin, LOW);
-    for(int x = 0; x < 80; x++) {
-      rotate_CW();
-    }
-    Serial.print(newPos);
-    Serial.println();
-    pos = newPos;
+  
+  if (destination != pos){
+    rotate(destination > pos);
   }
-if (Serial.available() > 0) {
-    newPos = Serial.parseInt(); 
-  }
-  if (newPos > pos) {
-    steps = newPos - pos;
-    for (int x=0; x < steps; x++) { 
-      rotate_CW();
-    }
-   pos = newPos;
-   encoder.setPosition(pos);
-   Serial.println(pos);
-  }
-  else if (newPos < pos) {
-    steps = pos - newPos;
-    for (int x=0; x < steps; x++){
-      rotate_CCW();
-    }
-    pos = newPos;
-    encoder.setPosition(pos);
-    Serial.println(pos);
-  }
-
 }
-
-
-void rotate_CW(){
-  digitalWrite(dirPin,HIGH);
+void rotate(int direction){
+    // TODO: read home pin, prevent moving in negative direction
+    digitalWrite(dirPin, direction);
     digitalWrite(stepPin, HIGH);
-      delayMicroseconds(500);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(500);
-}
-
-void rotate_CCW (){
-  if (digitalRead(homePin) {
-   digitalWrite (dirPin, LOW);
-    digitalWrite(stepPin, HIGH);
-      delayMicroseconds(500);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(500);
-  }
-  else {
-    pos = 0;
-  }
+    delayMicroseconds(100);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(100);
+    pos += 2 * direction - 1;
+    if (pos == destination){
+      Serial.println(pos);
+    }
 }
