6adac1d2c1834116fab55c4bf38599b8b03b2c77
Kyle Sunden <sunden@wisc.edu>
2019-06-14 10:48:49 -0500

    Update for yaq serial

--- motord/daemon.py
+++ motord/daemon.py
@@ -1,39 +1,30 @@
 #! /usr/bin/env python3
 import asyncio
 
-import serial
-from yaq_daemon_core import hardware
+import yaq_serial
+from yaqd_core import hardware
 
-class WlMotorDaemon(hardware.BaseHardwareDaemon):
+class WlMotorDaemon(hardware.ContinuousHardwareDaemon):
+    defaults = {"baudrate": 57600}
+
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
 
         self.limit_status = False
-        self._port = serial.Serial(config["com_port"], timeout=0, baudrate=config["baud_rate"])
+        self._port = yaq_serial.YaqSerial(config["com_port"], baudrate=config["baudrate"])
+        self._limits = config.get("limits", [(-100_000, 100_000)])
 
     def _set_position(self, position):
         self._port.write(str(position).encode() + b"\n")
 
     async def update_state(self):
         overflow = b""
-        while True:
-            if self._port.in_waiting:
-                message = self._port.read(self._port.in_waiting)
-                print(f"Update State: {message!r}")
-                try:
-                    message = overflow + message
-                    word = message.split(b"E")[-2]
-                    word = word.split(b"S")[-1]
-                    new_position = int(word)
-                    overflow = b""
-                except (ValueError, IndexError) as e:
-                    print("Exception occurred", e)
-                    overflow = message
-                    continue
-                self._not_busy.set()
-                self._position = new_position
-                self._destination = self._position
-            await asyncio.sleep(0.01)
+        async for line in self._port.areadlines():
+            print("UPDATE STATE", line)
+            line = line.replace(b"S", b"").replace(b"E",b"")
+            self._position = float(line)
+            self._not_busy.set()
+            self._destintation = self._position
 
 if __name__ == "__main__":
     WlMotorDaemon.main()
